f2e9f8a2ad42abfb599dd86d29c2cd235e03183a
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-12-21 20:43:57 +0530

    Commit Thala For A Reason.cpp

---
+++ Starters-113/Thala For A Reason.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    if(t==7)
+    cout << "THALA";
+    else
+    cout << "SADGE";
+    /*while(t--)
+    {
+        int n;
+        cin >> n;
+        
+    }*/
+	return 0;
+}
