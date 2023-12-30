aed0373ca2e9b11a0782500fb789000052c36ed0
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2023-12-30 15:11:17 +0530

    Commit Christmax_Greetings.cpp

---
+++ Starters-114/Christmas_Greetings.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t;
+	cin >> t;
+	t==25 ? cout << "CHRISTMAS" : cout << "ORDINARY";
+	return 0;
+}
